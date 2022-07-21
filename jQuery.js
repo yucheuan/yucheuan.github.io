568ebf034150f20de739b6906b7a052fbd05fd34
Yu Cheng Huang <cookiehuang310@gmail.com>
2022-07-20 18:59:55 -0700

    Add Files
    YCH
    
    -Add interface files

---
+++ jQuery.js
@@ -0,0 +1,83 @@
+/*Image Slider*/
+$('#phaseOne1').click(function() {
+    //alert("Refresh!"); 
+    $('.slide.first').css({
+        'margin-left': '-20%'
+    });
+
+    $('.videox.first').css({
+        'margin-left': '-16.7%'
+    });    
+
+    document.getElementById('phaseOneV2').play();
+});
+
+$('#phaseOne2').click(function() { 
+    $('.slide.first').css({
+        'margin-left': '-40%'
+    });
+
+    $('.videox.first').css({
+        'margin-left': '-33.4%'
+    });
+
+    document.getElementById('phaseOneV3').play();
+});
+
+$('#phaseOne3').click(function() {
+    $('.slide.first').css({
+        'margin-left': '-60%'
+    });
+
+    $('.videox.first').css({
+        'margin-left': '-50.1%'
+    });
+
+    document.getElementById('phaseOneV4').play();
+});
+
+$('#phaseOne4').click(function() {
+    $('.slide.first').css({
+        'margin-left': '-80%'
+    });
+
+    $('.videox.first').css({
+        'margin-left': '-66.8%'
+    });
+
+    document.getElementById('phaseOneV5').play();
+});
+
+$('#phaseOne5').click(function() {
+    $('.slide.first').css({
+        'margin-left': '-100%'
+    });
+
+    $('.videox.first').css({
+        'margin-left': '-83.5%'
+    });
+
+    document.getElementById('phaseOneV6').play();
+});
+
+$('#phaseOne6').click(function() {
+    $('.slide.first').css({
+        'margin-left': '0%'
+    });
+
+    $('.videox.first').css({
+        'margin-left': '0%'
+    });
+
+    document.getElementById('phaseOneV1').play();
+});
+
+/*Video Slider*/
+$('#phaseOneV1').click(function() {
+    //alert("Refresh!");
+    //$('#phaseOneV1')[0].play();
+
+    /*$('.video.first').css({
+        'margin-left': '-20%'
+    });*/
+});
